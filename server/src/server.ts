4d736d98e911efdd0860c058c2fd5795608884f9
unknown <henrique.almeida.ads@gmail.com>
2022-05-30 11:35:24 -0300

    deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json())
 app.use(routes)
 
-app.listen(3001, () => console.log('Rodando'));
+app.listen(process.env.PORT || 3001, () => console.log('Rodando'));
 
 
 
