0ce10807449687ee67f6606f455a1a07a272712b
User <henrique.almeida.ads@gmail.com>
2022-10-28 13:35:33 -0300

    .

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import { routes } from './routes';
 
 const app = express();
 
-app.use(cors({ origin: 'https://react-widget-2vza5e3sm-rikeealmeida.vercel.app/' }));
+app.use(cors({ origin: '*' }));
 app.use(express.json())
 app.use(routes)
 
